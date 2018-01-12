f0682e25b1ff28d0ef56bd2681a30131bab14454
Dario Dorella <dario.dorella@gmail.com>
2018-01-12 17:47:54 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,7 @@ function init() {
     // the Konami code, don't alert() anything and simply keep listening for all
     // ten keydowns in the correct order.
 
-    document.body.addEventListener('keydown', checkSequence(event,code,gotcha,displayAlert) {
+    document.body.addEventListener('keydown', checkSequence(event,code,gotcha,displayAlert)
 
 }
 
